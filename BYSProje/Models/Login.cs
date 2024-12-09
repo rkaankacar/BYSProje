564c54e757ce3ccc300ddd3cb48c8a57f37f5557
Rahim Kaan Kacar <rahimkaanis@hotmail.com>
2024-12-09 19:04:55 +0300

    login ve student düzenlendi

--- BYSProje/Models/Login.cs
+++ BYSProje/Models/Login.cs
@@ -7,6 +7,7 @@ namespace BYSProje.Models
 {
     public class Login
     {
-        
+        public int UserNo {get; set;}
+        public string Password {get; set;}
     }
 }
--- BYSProje/Models/Students.cs
+++ BYSProje/Models/Students.cs
@@ -7,6 +7,11 @@ namespace BYSProje.Models
 {
     public class Students
     {
-        
+        public int StudentID {get; set;}
+        public string First_Name {get; set;}
+        public string Last_Name {get; set;}
+        public string Email {get; set;}
+        public string Password {get; set;}
+        public string Major {get; set;}
     }
 }
