92d08f3da5233cc56ca3296469b8f9a1f84d51db
Rahim Kaan Kacar <rahimkaanis@hotmail.com>
2024-12-13 17:04:13 +0300

    hata bulma

--- BYSProje/Models/CourseViewModel.cs
+++ BYSProje/Models/CourseViewModel.cs
@@ -7,10 +7,10 @@ namespace BYSProje.Models
 {
     public class CourseViewModel
     {
-         public int CourseID { get; set; }
-         public string CourseName { get; set; }
-         public float Credits { get; set; }
-             public Courses Course { get; set; }
-         public Students Student { get; set; }
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public float Credits { get; set; }
+        public Courses Course { get; set; }
+        public Students Student { get; set; }
     }
 }
--- BYSProje/obj/Debug/net8.0/BYSProje.AssemblyInfo.cs
+++ BYSProje/obj/Debug/net8.0/BYSProje.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BYSProje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27ebfd8df54e067d87d90f7acad3b245ce71a67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da1976cce818402a6704a82d1a9d3afdfa7b4e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BYSProje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BYSProje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
